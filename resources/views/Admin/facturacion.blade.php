a1668954b65aec8147535e885f25fd614b6b40dc
ksama22 <ksama22@jaumebalmes.net>
2023-03-02 20:15:37 +0100

    Practica 13 - TRUCADES I NOMS A RUTES

--- resources/views/Admin/facturacion.blade.php
+++ resources/views/Admin/facturacion.blade.php
@@ -1,1 +1,2 @@
 El servidor Facturacion diu: '"{{$text}}"
+<a href="<?=route('link_home')?>">Home</a>
---
+++ resources/views/Admin/home.blade.php
@@ -0,0 +1,4 @@
+El servidor admin diu: '"{{$text}}"
+<a href="<?=route('link_cliente')?>">Cliente</a>
+<a href="<?=route('link_contabilidad')?>">Contabilidad</a>
+<a href="<?=route('link_facturacion')?>">Facturcion</a>
